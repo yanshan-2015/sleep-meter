67f8f50dc533cb173de15cc40b0ef1d7354d6c6b
Mr.Yuan <ysdn20z@163.com>
2017-07-18 16:39:29 +0800

    modify and add>>> modify: index--resolving img disappear//hipline--submit and the 'loading...'//report--list style change || add: login module--(checkCode) click event do bind and unbind,resolving send repeat

--- src/js/hipline.js
+++ src/js/hipline.js
@@ -6,23 +6,16 @@ import '../less/hipline.less'
 import '../less/loginModule.less'
 import { Button } from './component/buttonModule'
 import { LoginElement } from './component/loginModule'
+import { Loading } from './component/loading'
 
 import userImg from '../images/8-icon1.png'
 import phoneImg from '../images/8-icon2.png'
 import codeImg from '../images/8-icon3.png'
 
-// import getwx from './component/getWx'
 import getwx from './component/getWx';
 
 window.onload = function () {
-    let _this = this;
-    $('.img').click(function () {
-        getwx().then(function(res){
-            console.log(res)
-        }, function(res){
-            console.log(res)
-        });
-    });
+    //next button 配置参数
     let checkObj = {
         dom: document.getElementsByClassName('hipline')[0],
         show: 'block',
@@ -32,6 +25,7 @@ window.onload = function () {
         top: '4.5rem',
         bottom: '1.5rem'
     };
+    //登录界面配置参数
     let loginPram = {
         title: '快捷登录以查看测试结果',
         listNum: 3,
@@ -81,6 +75,7 @@ window.onload = function () {
     });
     let checkInfo = {};
     let button,login,inputElement = document.getElementById('hiplineW');
+    //next button 构造
     function createElement() {
         if(!button){
             button = new Button(checkObj);
@@ -89,7 +84,7 @@ window.onload = function () {
         }
     }
     inputElement.onfocus = function () {
-        //生成btn
+        //生成next button
         createElement();
         let nextButton = document.getElementById('nextButton');
         nextButton.onclick = function () {
@@ -98,89 +93,51 @@ window.onload = function () {
                 //生成用户信息验证码界面
                 login = new LoginElement(loginPram);
                 //验证界面的两个行为
-                submitBtn(); //提交处理
-                checkCode(); //验证码处理
+                $('#submit').bind('click',handleSubmitBtn); //提交处理
+                $('#codeSpan').bind('click',handleCheckCode); //验证码处理
             }
         };
     };
-
-    function submitBtn() {
-        let submitBtn = document.getElementById('submit');
-        submitBtn.onclick = function () {
-            checkInfo.phone = $('#phone').val();
-            checkInfo.name = $('#name').val();
-            checkInfo.code = $('#code').val();
-            //检测用户信息
-            let condition = login.checkInfo(checkInfo);
-            if(condition === false){
-                return
-            }
-            if(condition === true){
-                $('#phone').val('');
-                return
-            }
-            //对code进检测
-            let param = {
-                url: 'http://zs.derucci.net/deruccimid/antifake/verifymsgcode',
-                data: {
-                    mobile: checkInfo.phone,
-                    verifyCode: checkInfo.code,
-                }
-            };
-            login.postAjax(param,function (data) {
-                if(data.success !== true){
-                    alert('验证码无效或过期');
-                    return
-                }
-                ajaxParam.data.telphone = checkInfo.phone;
-                ajaxParam.data.name = checkInfo.name;
-                login.postAjax(ajaxParam,function (data) {
-                    if(data.msg === 'OK'){
-                        alert('数据已提交');
-                        console.log(ajaxParam);
-                        location.href = 'report.html';
-                    }else {
-                        alert('数据库异常，请尝试重新提交');
-                    }
-                })
-            });
-        };
-    }
-    function checkCode() {
-        let checkCode = document. getElementById('codeSpan');
-        checkCode.onclick = function (event) {
-            if(event.stopPropagation){
-                event.stopPropagation();
-            }else{
-                event.cancelBubble = true;
-            }
-            checkInfo.phone = $('#phone').val();
-            //做检测
-            if(login.checkCode(checkInfo) === false){
-                $('#phone').val('');
-            }
-            //调接口
-            let param = {
-                url: 'http://zs.derucci.net/deruccimid/antifake/sendmsg',
-                data: {
-                    mobile: checkInfo.phone
-                }
-            };
-            login.postAjax(param,function (data) {
-                codeItem(data,checkCode);
-            })
+    //或者验证码
+    function handleCheckCode() {
+        checkInfo.phone = $('#phone').val();
+        //做检测
+        if(login.checkCode(checkInfo) === false){
+            $('#phone').val('');
+            return
         }
+        let loading = new Loading();
+        loading.createEle();
+        loading.showEle();
+        //调接口
+        let param = {
+            url: 'http://zs.derucci.net/deruccimid/antifake/sendmsg',
+            data: {
+                mobile: checkInfo.phone
+            }
+        };
+        login.postAjax(param,function (data) {
+            loading.hideEle();
+            let codeSpan = document.getElementById('codeSpan');
+            codeItem(data,codeSpan);
+        });
     }
+    //验证码返回状态筛选
     function codeItem(x,y) {
         switch (x.code){
             case 0: setTime(1000,120,y) ;
             break;
-            case 1: alert('请求参数错误');
+            case 1: alert('请求参数错误，请重试');
             break;
-            case 2: alert('两分钟内只能发送一次');
+            case 2: alert('两分钟内只能发送一次短信');
+            break;
+            default: alert('暂未知');
+            break;
         }
     }
-    function setTime(n,m,dom,e) {
+    //验证码定时器
+    function setTime(n,m,dom) {
+        $('#codeSpan').unbind('click',handleCheckCode);
         dom.innerHTML = m;
         dom.style.backgroundColor = '#e1e1e1';
         let time = setInterval(function () {
@@ -190,9 +147,58 @@ window.onload = function () {
                 clearInterval(time);
                 dom.innerHTML = '重新发送';
                 dom.style.backgroundColor = '#fff';
+                $('#codeSpan').bind('click',handleCheckCode);
                 return
             }
         },n)
 
+    }
+    //提交数据到后台
+    function handleSubmitBtn() {
+        checkInfo.phone = $('#phone').val();
+        checkInfo.name = $('#name').val();
+        checkInfo.code = $('#code').val();
+
+        //对以上用户信息进行空值检测和正则检测
+        let condition = login.checkInfo(checkInfo);
+        if(condition === false){
+            return
+        }
+        if(condition === true){
+            $('#phone').val('');
+            return
+        }
+
+        //对code进行检测
+        let param = {
+            url: 'http://zs.derucci.net/deruccimid/antifake/verifymsgcode',
+            data: {
+                mobile: checkInfo.phone,
+                verifyCode: checkInfo.code,
+            }
+        };
+        login.postAjax(param,function (data) {
+            //回调函数进行检测
+            if(data.success !== true){
+                alert('验证码无效或过期');
+                return
+            }
+            let loading = new Loading();
+            loading.createEle();
+            loading.showEle();
+
+            //回调函数无异常，最终提交数据如下
+            ajaxParam.data.telphone = checkInfo.phone;
+            ajaxParam.data.name = checkInfo.name;
+            login.postAjax(ajaxParam,function (data) {
+                loading.hideEle();
+                if(data.msg === 'OK'){
+                    alert('数据已提交');
+                    location.href = 'report.html';
+                }else {
+                    alert('数据库异常，请尝试重新提交');
+                }
+            })
+        });
     }
 };
